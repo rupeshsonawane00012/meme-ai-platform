9bb376427aeab59e99a59d23b174cbbe8066a8a9
Rupesh  Sonawane <148001273+rupeshsonawane00012@users.noreply.github.com>
2026-05-12 02:53:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
+import React from "react";
+
 function App() {
   return (
-    <div style={{padding: "40px", fontFamily: "Arial"}}>
-      <h1>Meme AI Platform</h1>
-      <p>Production DevOps Practice Project</p>
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundColor: "#111827",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1 style={{ fontSize: "48px" }}>
+        Meme AI Platform 🚀
+      </h1>
+
+      <p style={{ marginTop: "20px" }}>
+        Successfully deployed on AWS EC2 using Docker
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
